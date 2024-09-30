8532267c80027a1e4596fb39119e3777f972c87c
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-09-30 11:16:04 +0200

    make Y work

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- https://www.reddit.com/r/neovim/comments/petq61/neovim_060_y_not_yanking_line_but_to_end_of_line/
+vim.cmd("nnoremap Y Y")
